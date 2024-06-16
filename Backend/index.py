0d8c2310866749992bfeb586dc3dab13f2a74041
Samman L <lamsal.samman@gmail.com>
2024-06-16 16:58:12 -0400

    Implemented functionality to allow websockets to emit and handle data

---
+++ Backend/index.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import socketio
+
+class AllowedUsers:
+    def __init__(self, user1, user2):
+        self.user1 = user1
+        self.user2 = user2
+
+allowed_users = AllowedUsers("user1", "user2")
+connected_clients = {}
+
+app = FastAPI()
+origins = ['https://localhost:3000']
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
+sio_server = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins=[])
+sio_app = socketio.ASGIApp(socketio_server=sio_server)
+
+app.mount("/", sio_app)
+
+@sio_server.event
+async def message(sid, message):
+    print(f"Message from {sid}")
+    await sio_server.send(message)
+
+if __name__ == "__main__":
+    uvicorn.run("index:app", host="127.0.0.1", port = 8000, reload=True)
--- React/messaging-app/app/ChatScreen.tsx
+++ React/messaging-app/app/ChatScreen.tsx
@@ -5,6 +5,8 @@ import {
   ScrollView,
   TextInput,
   KeyboardAvoidingView,
+  Pressable,
+  Image,
 } from "react-native";
 import React, { useEffect, useState, useRef } from "react";
 import ChatBubble from "@/components/ChatBubble";
@@ -12,20 +14,30 @@ import { Input } from "@rneui/themed";
 import { io } from "socket.io-client";
 
 const socket = io("ws://localhost:8000");
-//const [msg, setMsg] = useState("hi");
 
 socket.on("connect", () => {
   console.log("connected");
 });
 
-socket.on("message", () => {});
+socket.on("message", (data) => {
+  console.log(data);
+  // create chatbubble component
+});
 
 const ChatScreen = () => {
+  const [message, setMessage] = useState("");
+
   return (
     <KeyboardAvoidingView style={styles.view}>
-      {/* <ChatBubble message={msg} isSender={true}></ChatBubble> */}
       <KeyboardAvoidingView style={styles.sendMessageTextBox}>
-        <Input placeholder="Message" />
+        <Input
+          placeholder="Message"
+          value={message}
+          onChangeText={(text) => setMessage(text)}
+        />
+        <Pressable onPressIn={() => socket.emit("message", message)}>
+          <Image source={require("../img/send_icon.svg")} />
+        </Pressable>
       </KeyboardAvoidingView>
     </KeyboardAvoidingView>
   );
