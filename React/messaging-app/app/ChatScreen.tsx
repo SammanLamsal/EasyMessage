db95255bb4c2e33b44ad9b0fdc30e0a9df8839f1
Samman L <lamsal.samman@gmail.com>
2024-06-17 18:29:35 -0400

    Allowed generating chat bubbles on click

--- React/messaging-app/app/ChatScreen.tsx
+++ React/messaging-app/app/ChatScreen.tsx
@@ -12,30 +12,45 @@ import React, { useEffect, useState, useRef } from "react";
 import ChatBubble from "@/components/ChatBubble";
 import { Input } from "@rneui/themed";
 import { io } from "socket.io-client";
+import { ListItem } from "@rneui/base";
 
 const socket = io("ws://localhost:8000");
+const chatsOnScreen: String[] = [];
 
 socket.on("connect", () => {
   console.log("connected");
 });
 
-socket.on("message", (data) => {
-  console.log(data);
-  // create chatbubble component
+socket.on("message", (data: string) => {
+  chatsOnScreen.push(data);
 });
-
+// TODO: make it so users can't send empty strings
 const ChatScreen = () => {
   const [message, setMessage] = useState("");
+
+  const chatItems = chatsOnScreen.map((chat) => (
+    <ChatBubble message={String(chat)} isSender={true} />
+  ));
 
   return (
     <KeyboardAvoidingView style={styles.view}>
       <KeyboardAvoidingView style={styles.sendMessageTextBox}>
+        <ListItem style={styles.chatBubbles}>
+          <ListItem.Content>{chatItems}</ListItem.Content>
+        </ListItem>
         <Input
           placeholder="Message"
           value={message}
           onChangeText={(text) => setMessage(text)}
         />
-        <Pressable onPressIn={() => socket.emit("message", message)}>
+        <Pressable
+          onPressIn={() => {
+            if (message) {
+              socket.emit("message", message);
+            }
+          }}
+          onPressOut={() => setMessage("")}
+        >
           <Image source={require("../img/send_icon.svg")} />
         </Pressable>
       </KeyboardAvoidingView>
@@ -53,4 +68,5 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 0,
   },
+  chatBubbles: {},
 });
